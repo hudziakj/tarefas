9f6aa6daf7c48f0ea39553c53dfe647c80caf19c
Matheus Hudziak <matheuszhudziak@gmail.com>
2022-12-15 10:15:37 -0300

    Adicionando arrow functions

--- lampada/script.js
+++ lampada/script.js
@@ -3,18 +3,28 @@ let turnOff = document.getElementById("turnOff");
 let imagem = document.getElementById("imagem");
 let roubar = document.getElementById("roubar");
 
-function ligar() {
-  imagem.src = "svg/lamp-on.svg";
-}
+// function ligar() {
+//   imagem.src = "svg/lamp-on.svg";
+// }
 
-function desligar() {
-  imagem.src = "svg/lamp-off.svg";
-}
+// function desligar() {
+//   imagem.src = "svg/lamp-off.svg";
+// }
 
-function sumir() {
-  imagem.src = "svg/lamp-roubar.svg";
-}
+// function sumir() {
+//   imagem.src = "svg/lamp-roubar.svg";
+// }
 
-turnOn.addEventListener("click", ligar);
-turnOff.addEventListener("click", desligar);
-roubar.addEventListener("click", sumir);
+turnOn.addEventListener(
+  "click",
+  (ligar = () => (imagem.src = "svg/lamp-on.svg"))
+);
+
+turnOff.addEventListener(
+  "click",
+  (desligar = () => (imagem.src = "svg/lamp-off.svg"))
+);
+roubar.addEventListener(
+  "click",
+  (sumir = () => (imagem.src = "svg/lamp-roubar.svg"))
+);
